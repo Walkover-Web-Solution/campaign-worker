d3d48a1b6e957efc1753b8eea3870e46acccd555
Ravi Soni <ravisoni@walkover.in>
2022-03-24 19:53:43 +0530

    Ravi | config clear and testing pipeline

--- .platform/hooks/postdeploy/run_caches.sh
+++ .platform/hooks/postdeploy/run_caches.sh
@@ -5,8 +5,10 @@
 # After the deployment, it's highly recommended
 # to re-run the caches for config, routes and views.
 
-php artisan config:cache
+# php artisan config:cache
 
 php artisan route:cache
 
 php artisan view:cache
+
+php artisan config:clear
