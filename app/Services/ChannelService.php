428c84e148d772948335a5a2f0d5239345353cde
paaresh-jaisinghani <paresh@whozzat.com>
2022-03-28 20:15:15 +0530

    Paresh | done with sms:consume

--- app/Services/ChannelService.php
+++ app/Services/ChannelService.php
@@ -13,6 +13,8 @@ use App\Models\Campaign;
 use App\Models\Company;
 use App\Models\FlowAction;
 use App\Models\Template;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use MongoDB\Model\BSONArray;
 use MongoDB\Model\BSONDocument;
 
@@ -28,49 +30,104 @@ class ChannelService
         $this->mongo = new MongoDBLib;
     }
 
-    public function sendData($campid, $flowid, $mongoid, $actionid)
+    public function sendData($actionLogId)
     {
+        Log::debug('For ' . $actionLogId);
+        $action_log = ActionLog::where('id', $actionLogId)->first();
 
-        $flow = FlowAction::where('campaign_id', $campid)->where('id', $flowid)->first();
-        if ($flow['linked_type'] == 'App\Models\Condition') {
-            $flow = FlowAction::where('campaign_id', $campid)->where('parent_id', $flow['id'])->first()->toarray();
-            $this->sendData($campid, $flow, $mongoid, $actionid);
+        /**
+         * generating the token
+         */
+        $campaign = Campaign::find($action_log->campaign_id);
+        $input['company'] = $campaign->company;
+        config(['msg91.jwt_token' => createJWTToken($input)]);
+
+        $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('id', $action_log->flow_action_id)->first();
+
+
+        // if ($flow['linked_type'] == 'App\Models\Condition') {
+        //     $flow = FlowAction::where('campaign_id', $campid)->where('parent_id', $flow->id)->first()->toarray();
+        //     $this->sendData($campid, $flow, $mongoid, $actionid);
+        // } else {
+        /**
+         * geting the data from the mongoDB
+         */
+        $data = $this->mongo->collection('run_campaign_data')->findOne(collect($action_log->mongo_id));
+        $md = json_decode(json_encode($data['data']));
+        $lib = $this->setLibrary($flow['linked_id']);    //geting the object of the library
+        $temp = Template::where('flow_action_id', $flow['id'])->first();
+        $flag = 0;
+        if ($flow['linked_id'] == 1) {
+            $data = array(
+                'to' => $md[0]->to,
+                'from' => $md[0]->to,
+                'cc' => $md[0]->cc,
+                'bcc' => $md[0]->bcc,
+                'variables' => $md[0]->variables
+            );
+            $flag = 1;
         } else {
-            /**
-             * geting the data from the mongoDB
-             */
-            $data = $this->mongo->collection('run_campaign_data')->findOne(collect($mongoid));
-            $md = json_decode(json_encode($data['data']));
-            $lib = $this->setLibrary($flow['linked_id']);    //geting the object of the library
-            $temp = Template::where('flow_action_id', $flow['id'])->first();
-            $flag = 0;
-            if ($flow['linked_id'] == 1) {
-                $data = array(
-                    'to' => $md[0]->to,
-                    'from' => $md[0]->to,
-                    'cc' => $md[0]->cc,
-                    'bcc' => $md[0]->bcc,
-                    'variables' => $md[0]->variables
+            $data = [
+                "flow_id" => "611f7d5744b035602c46cb47",
+                'recipients' => $md[0]->mobiles
+            ];
+            $flag = 2;
+        }
 
-                );
-                $flag = 1;
-            } else {
-                $data = [
-                    "flow_id" => "611f7d5744b035602c46cb47",
-                    'recipients' => $md[0]->mobiles
-                ];
-                $flag = 2;
-            }
-            $res = $lib->send($data);
-            if ($flag == 1) {
+        $res = $lib->send($data);
 
+        if ($flag == 1) {
+            //
+        } else if ($flag = 2) {
+            $action = ActionLog::where('id', $action_log->id)->first();
+            $action->update(['ref_id' => $res->message]);
+        } else {
+            //
+        }
+
+        $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('parent_id', $flow->id)->first();
+        if (!empty($flow)) {
+            while ($flow['linked_type'] == 'App\Models\Condition') {
+                $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('parent_id', $flow->id)->first();
+                if (empty($flow)) {
+                    return;
+                }
             }
-            else if ($flag = 2)
-                ActionLog::where('campaign_id', $campid)->where('flow_action_id', $flowid)->update(['ref_id' => $res->message]);
-            else {
-            }
+            $actionLogData = [
+                "no_of_records" => $action_log->no_of_records,
+                "ip" => request()->ip(),
+                "status" => "",
+                "reason" => "",
+                "ref_id" => "",
+                "flow_action_id" => $flow->id,
+                "mongo_id" => $action_log->mongo_id
+            ];
+            $action_log = $campaign->actionLogs()->create($actionLogData);
+            Log::debug('Found next action log ' . $actionLogId);
+            $this->sendData($action_log->id);
+        } else {
             return;
         }
+        // while ($flow['linked_type'] == 'App\Models\Condition') {
+        //     $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('parent_id', $flow->id)->first()->toarray();
+        // if (empty($flow)) {
+        //     return;
+        // }
+        // }
+        // Log::debug('Found next action log ' . $flow->id);
+        // if (!empty($flow)) {
+        // $actionLogData = [
+        //     "no_of_records" => $action_log->no_of_records,
+        //     "ip" => request()->ip(),
+        //     "status" => "",
+        //     "reason" => "",
+        //     "ref_id" => "",
+        //     "flow_action_id" => $flow->id,
+        //     "mongo_id" => $action_log->mongo_id
+        // ];
+        // $action_log = $campaign->actionLogs()->create($actionLogData);
+        // }
+
     }
 
 
