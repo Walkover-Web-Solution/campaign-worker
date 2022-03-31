fdae99472f3f638ce042e78da5a19780234cc0c4
Ravi Soni <ravisoni@walkover.in>
2022-03-31 19:11:36 +0530

    Ravi | eb experiment 0.6

--- .platform/hooks/prebuild/00_permission_to_files.sh
+++ .platform/hooks/prebuild/00_permission_to_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo chmod +x .platform/hooks/prebuild/*.sh
+# sudo chmod +x .platform/hooks/prebuild/*.sh
 
-sudo chmod +x .platform/hooks/postdeploy/*.sh
+# sudo chmod +x .platform/hooks/postdeploy/*.sh
