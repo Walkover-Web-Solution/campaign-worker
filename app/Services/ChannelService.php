8ab68d3ccc3ef9d53dd856bff11e31a09712b107
paaresh-jaisinghani <paresh@whozzat.com>
2022-04-07 20:26:28 +0530

    Paresh |  Divide the code into a different function in  channel service

--- app/Services/ChannelService.php
+++ app/Services/ChannelService.php
@@ -39,6 +39,7 @@ class ChannelService
     {
 
         $action_log = ActionLog::where('id', $actionLogId)->first();
+
         /**
          * generating the token
          */
@@ -54,56 +55,13 @@ class ChannelService
         $data = $this->mongo->collection('run_campaign_data')->find([
             'requestId' => $action_log->mongo_id
         ]);
-
         $md = json_decode(json_encode($data));
-        $temp = Template::where('flow_action_id', $flow['id'])->first();  //geting the template information according to flow
 
-        /**
-         * extracting the all the variables from the mongo data
-         */
-        $var = collect($md[0]->data);
-        unset($var['emails']);
-        unset($var['mobiles']);
-        unset($var['mobile']);
-        $variables = collect($var)->map(function ($value, $key) use ($temp) {
-            if (in_array($key, $temp->variables)) {
-                return $value;
-            }
-        });
-        $variables = array_filter($variables->toArray());
-
-        $flag = 0;
-        $obj = new \stdClass();
-        $obj->data = [];
-        $mongo_data = collect($md[0]->data);
-        $channel = ChannelType::where('id', $flow['channel_id'])->first();
-        $conditions = $channel->conditions()->pluck('name')->toArray(); //generating an array of all the condition belong to flow channel id
         /**
          * generating the request body data according to flow channel id
          */
-        switch ($flow['channel_id']) {
-            case 1:
-                array_push($obj->data, $mongo_data['emails']);
-                $data = array(
-                    'variables' => $variables,
-                    'template_id' => $temp->template_id
-                );
-                $data = array_merge(collect($obj->data[0])->toArray(), $data);
-                $flag = 1;
-                break;
-            case 2:
-                array_push($obj->data, $mongo_data['mobiles']);
-                $data = [
-                    "flow_id" => $temp->template_id,
-                    'recipients' => $obj->data[0],
-                    "short_url" => true
-                ];
-                $flag = 2;
-                break;
-            case 3:
-                array_push($obj->data, $mongo_data['mobile']); //for otp
-                break;
-        }
+        $data = $this->getRequestBody($flow, $md);
+
         /**
          * Geting the libary object according to the flow channel id to send the data to the microservice
          */
@@ -112,69 +70,16 @@ class ChannelService
         /**
          * updateing the responce comes from the microservice into the ref_id of current flow action
          */
-        if ($flag == 1) {
-            $val = $res->data->unique_id;
-        } else if ($flag = 2) {
-            $val = $res->data;
-        } else {
-            //
-        }
-        $action = ActionLog::where('id', $action_log->id)->first();
-        $action->update(['ref_id' => $val]);
+        $new_action_log = $this->updateActionLogResponse($flow, $action_log, $res);
 
-        /**
-         *  geting the next flow id according to the responce status from microservice
-         */
-        $status = ucfirst($res->status);
-        $next_flow_id = null;
-        if ($status == 'Success')
-            $next_flow_id = $flow->module_data->op_success;
-        else
-            $next_flow_id = $flow->module_data->op_failure;
-        if (in_array($status, $conditions) && !empty($next_flow_id)) {
-            $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('id', $next_flow_id)->first();
-            if (!empty($flow)) {
-                $actionLogData = [
-                    "campaign_id" => $action_log->campaign_id,
-                    "no_of_records" => $action_log->no_of_records,
-                    "ip" => request()->ip(),
-                    "status" => "pending",
-                    "reason" => "",
-                    "ref_id" => "",
-                    "flow_action_id" => $next_flow_id,
-                    "mongo_id" => $action_log->mongo_id
-                ];
-                $actionLog = $campaign->actionLogs()->create($actionLogData);
-
-                $channel_id = FlowAction::where('id', $actionLogData['flow_action_id'])->pluck('channel_id')->first();
-                //selecting the queue name as per the flow channel id
-                switch ($channel_id) {
-                    case 1:
-                        $queue = 'run_email_campaigns';
-                        break;
-                    case 2:
-                        $queue = 'run_sms_campaigns';
-                        break;
-                    case 3:
-                        $queue = 'run_otp_campaigns';
-                        break;
-                    case 4:
-                        $queue = 'run_whastapp_campaigns';
-                        break;
-                    case 5:
-                        $queue = 'run_voice_campaigns';
-                        break;
-                }
-
-                $input = new \stdClass();
-                $input->action_log_id =  $actionLog->id;
-                RabbitMQJob::dispatch($input)->onQueue($queue); //dispatching the job
-            }
+        if (!empty($new_action_log)) {
+            $input = new \stdClass();
+            $input->action_log_id =  $new_action_log->id;
+            $channel_id = FlowAction::where('id', $new_action_log->flow_action_id)->pluck('channel_id')->first();
+            $this->createNewJob($channel_id, $input);
         }
         return;
     }
-
-
 
     public function setLibrary($channel)
     {
@@ -195,5 +100,125 @@ class ChannelService
             case $voice:
                 return new VoiceLib();
         }
+    }
+
+    public function getRequestBody($flow, $md)
+    {
+        /**
+         * extracting the all the variables from the mongo data
+         */
+        $var = collect($md[0]->data);
+        unset($var['emails']);
+        unset($var['mobiles']);
+        unset($var['mobile']);
+        $obj = new \stdClass();
+        $obj->data = [];
+        $temp = Template::where('flow_action_id', $flow['id'])->first();  //geting the template information according to flow
+        $variables = collect($var)->map(function ($value, $key) use ($temp) {
+            if (in_array($key, $temp->variables)) {
+                return $value;
+            }
+        });
+        $variables = array_filter($variables->toArray());
+        $mongo_data = collect($md[0]->data);
+        switch ($flow['channel_id']) {
+            case 1:
+                if (!isset($this->mongo_data['emails']->cc)) {
+                    $cc = $flow['configurations']->cc;
+                }
+                if (!isset($mongo_data['emails']->bcc)) {
+                    $bcc = $flow['configurations']->bcc;
+                }
+                $data = array(
+                    "to" => $mongo_data['emails']->to,
+                    "from" => $flow['configurations']->from,
+                    "cc" => $cc,
+                    "bcc" => $bcc,
+                    'variables' => $variables,
+                    'template_id' => $temp->template_id
+                );
+                break;
+            case 2:
+                array_push($obj->data, $mongo_data['mobiles']);
+                $data = [
+                    "flow_id" => $temp->template_id,
+                    'recipients' => $obj->data[0],
+                    "short_url" => true
+                ];
+                break;
+            case 3:
+                array_push($obj->data, $mongo_data['mobile']); //for otp
+                break;
+        }
+        return $data;
+    }
+
+    public function updateActionLogResponse($flow, $action_log, $res)
+    {
+        if ($flow->channel_id == 1) {
+            $val = $res->data->unique_id;
+        } else if ($flow->channel_id = 2) {
+            $val = $res->data;
+        } else {
+            //
+        }
+        $action = ActionLog::where('id', $action_log->id)->first();
+        $action->update(['ref_id' => $val]);
+        $channel = ChannelType::where('id', $flow->channel_id)->first();
+        $conditions = $channel->conditions()->pluck('name')->toArray(); //generating an array of all the condition belong to flow channel id
+
+        $campaign = Campaign::find($action_log->campaign_id);
+        /**
+         *  geting the next flow id according to the responce status from microservice
+         */
+
+        $status = ucfirst($res->status);
+        $next_flow_id = null;
+        if ($status == 'Success')
+            $next_flow_id = $flow->module_data->op_success;
+        else
+            $next_flow_id = $flow->module_data->op_failure;
+
+        if (in_array($status, $conditions) && !empty($next_flow_id)) {
+            $flow = FlowAction::where('campaign_id', $action_log->campaign_id)->where('id', $next_flow_id)->first();
+            if (!empty($flow)) {
+                $actionLogData = [
+                    "campaign_id" => $action_log->campaign_id,
+                    "no_of_records" => $action_log->no_of_records,
+                    "ip" => request()->ip(),
+                    "status" => "pending",
+                    "reason" => "",
+                    "ref_id" => "",
+                    "flow_action_id" => $next_flow_id,
+                    "mongo_id" => $action_log->mongo_id
+                ];
+                $actionLog = $campaign->actionLogs()->create($actionLogData);
+                return $actionLog;
+            }
+        }
+        return;
+    }
+
+    public function createNewJob($channel_id, $input)
+    {
+        //selecting the queue name as per the flow channel id
+        switch ($channel_id) {
+            case 1:
+                $queue = 'run_email_campaigns';
+                break;
+            case 2:
+                $queue = 'run_sms_campaigns';
+                break;
+            case 3:
+                $queue = 'run_otp_campaigns';
+                break;
+            case 4:
+                $queue = 'run_whastapp_campaigns';
+                break;
+            case 5:
+                $queue = 'run_voice_campaigns';
+                break;
+        }
+        RabbitMQJob::dispatch($input)->onQueue($queue); //dispatching the job
     }
 }
