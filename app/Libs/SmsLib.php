3e3c28e602d1d02dd59944308d1b882ff5704365
Ravi Soni <ravisoni@walkover.in>
2022-04-19 18:57:03 +0530

    Ravi | removed unnecessary code from sms and email lib

--- app/Libs/SmsLib.php
+++ app/Libs/SmsLib.php
@@ -64,59 +64,5 @@ class SmsLib
         );
         logTest("sms response", $logData);
         return $res;
-
-        if (isset($res->hasError) && !empty($res->hasError)) {
-            $errorMsg = '';
-            collect($res->errors)->each(function ($error, $key) use (&$errorMsg) {
-                if (is_array($error)) {
-                    $error = $error[0];
-                }
-                if (empty($errorMsg)) {
-                    if (empty($key) || is_int($key)) {
-                        $errorMsg = $error;
-                    } else {
-                        $errorMsg = $key . ':' . $error;
-                    }
-                } else {
-                    if (is_int($key)) {
-                        $errorMsg = $error;
-                    } else {
-                        $errorMsg = $errorMsg . ",$key:" . $error;
-                    }
-                }
-            });
-            throw new \Exception($errorMsg, 1);
-        }
-
-
-        if (isset($res->type) && $res->type == 'error') {
-            if (isset($res->message)) {
-                throw new \Exception($res->message);
-            }
-            if (isset($res->msg)) {
-                throw new \Exception($res->msg);
-            }
-            throw new \Exception(json_encode($res));
-        }
-
-
-        if (isset($res->status) && $res->status == 'fail') {
-            $errors = [];
-            if (is_object($res->errors)) {
-                foreach ($res->errors as $error) {
-                    $errors[] = $error[0];
-                }
-            } else {
-                $errors = $res->errors;
-            }
-
-            throw new \Exception(implode(',', $errors), 1);
-        }
-
-        if (isset($res->msgType) && $res->msgType == 'error') {
-            throw new \Exception($res->msg, 1);
-        }
-
-        return $res;
     }
 }
