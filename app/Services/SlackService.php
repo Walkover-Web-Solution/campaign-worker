7d0d9df3c4148a199871110273571b99898745e2
jprasuk112 <97219996+jprasuk112@users.noreply.github.com>
2022-06-24 19:11:02 +0530

    Handle loop (#34)
    Reviewed

--- app/Services/SlackService.php
+++ app/Services/SlackService.php
@@ -131,6 +131,35 @@ class SlackService
     }
 
 
+    public function sendLoopErrorToSlack($error)
+    {
+        $input = array(
+            'text' => 'Loop Detected Error',
+            "attachments" => [
+                [
+                    "title" => "Action_log_id",
+                    "value" => $error->Action_log_id,
+                    "short" => false,
+                ]
+            ]
+        );
+
+        if (isset($error->code)) {
+            $input['attachments'][0]['fields'][] = [
+                'title' => 'Code',
+                'value' => $error->code,
+                'short' => false,
+            ];
+        }
+
+        // $response = Curl::to('https://hooks.slack.com/services/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')
+        $response = Curl::to('https://hooks.slack.com/services/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')
+            ->withData($input)
+            ->asJson()
+            ->post();
+    }
+
+
     public function sendAPILog($input)
     {
         $input = array(
